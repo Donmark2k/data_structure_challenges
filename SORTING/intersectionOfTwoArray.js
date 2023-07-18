d379c5f194c28061c0dba1e6868dc08ddb4431af
Donmark2k <chukwuemekavp@gmail.com>
2023-07-18 11:42:49 +0100

    Return point of intersection

---
+++ SORTING/intersectionOfTwoArray.js
@@ -0,0 +1,32 @@
+/*
+Intersection of Two Arrays
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+*/
+
+const intersection = function(nums1, nums2) {
+    const result = []
+    for (const num of nums2) {
+        if(nums1.includes(num)) {
+            result.push(num)
+            nums1.splice(nums1.indexOf(num),1)
+        }
+    }
+    return result
+}
+
+const nums1 = [4,9,5]
+const nums2 = [9,4,9,8,4]
+
+const intersect = intersection(nums1, nums2)
+console.log(intersect)
